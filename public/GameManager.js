232d3e5ac19a31417df2a0087f1e0c13bfd3c4e7
jbax1899 <jbax1899@users.noreply.github.com>
2024-07-26 19:03:56 -0400

    global playerColors

--- public/GameManager.js
+++ public/GameManager.js
@@ -1,8 +1,14 @@
 class GameManager {
-    constructor(scene, board, players, ui) {
+    playerColors = [
+        { color: 'FF0000', name: 'red' },
+        { color: '0000FF', name: 'blue' },
+        { color: '00FF00', name: 'green' },
+        { color: 'FFFF00', name: 'yellow' }
+    ];
+
+    constructor(scene, board, ui) {
         this.scene = scene;
         this.board = board;
-        this.players = players;
         this.ui = ui;
     }
 
